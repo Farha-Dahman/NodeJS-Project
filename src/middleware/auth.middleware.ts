f9fec5f2f9ba2b0c0edbb1e8f6f8f24894b5e52f
Farha Dahman <farhajehad@gmail.com>
2023-11-07 10:43:19 +0200

    adding unauthorized status code

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -7,11 +7,11 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { authorization } = req.headers;
     if (!authorization?.startsWith(process.env.BEARER_KEY!)) {
-      return res.json({ message: 'invalid token' });
+      return res.status(401).json({ message: 'Invalid token' });
     }
     const token = authorization.split(process.env.BEARER_KEY!)[1];
     if (!token) {
-      return res.json({ message: 'invalid token' });
+      return res.status(401).json({ message: 'Invalid token' });
     }
     const decoded = jwt.verify(token, process.env.LOGIN_SIGNATURE!) as JwtPayload;
     const userRepository = getRepository(User);
@@ -21,7 +21,7 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
       select: ['fullName', 'email'],
     });
     if (!authUser) {
-      return res.json({ message: 'Not register user' });
+      return res.status(401).json({ message: 'Not registered user' });
     }
     const user = authUser;
     (req as any).user = user;
