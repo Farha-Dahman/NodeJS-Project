3823bae9fc9e0cf5ea36c9c319d5e4b3c603460c
Farha Dahman <farhajehad@gmail.com>
2023-11-07 09:03:55 +0200

    update the app routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,29 @@
-export const AppRoutes = [
+import { Request, Response } from 'express';
+import authRouter from './router/auth.router';
+import userRouter from './router/user.router';
 
+export const AppRoutes = [
+  {
+    path: '/',
+    method: 'all',
+    action: (req: Request, res: Response) => {
+      res.json({ message: 'welcome!' });
+    },
+  },
+  {
+    path: '/auth',
+    method: 'use',
+    action: authRouter,
+  },
+  {
+    path: '/user',
+    method: 'use',
+    action: userRouter,
+  },  {
+    path: '*',
+    method: 'all',
+    action: (req: Request, res: Response) => {
+      res.json({ message: 'page not found' });
+    },
+  },
 ];
