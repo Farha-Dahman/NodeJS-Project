dbade34b12d584e033472269bb89050756a70292
Farha Dahman <farhajehad@gmail.com>
2023-11-08 15:32:49 +0200

    update the logger file

--- logger.ts
+++ logger.ts
@@ -1,7 +1,7 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL,
+  level: process.env.LOG_LEVEL || 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.File({ filename: 'combined.log' }),
